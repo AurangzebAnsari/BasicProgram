fcf6fdc0a5cb203ebc6fb7f70c746f2b0f93ecc5
AurangzebAnsari <ansari95904@gmail.com>
2016-12-09 13:34:16 -0800

    Example of Collection
    Example of ArrayList Class,
    Example of for each loop
    Example of User defined ArrayList
    Example of addAll() Method
    Example of remove() Method
    Example of retainAll() method

---
+++ JavaBasicAndOOPsProgram/src/CollectionExample/UserDefinedCollection/UserDefinedClass.java
@@ -0,0 +1,19 @@
+package CollectionExample.UserDefinedCollection;
+
+public class UserDefinedClass {
+	
+	int rollno;
+	String name;
+	int age;
+	public UserDefinedClass(int rollno,String name,int age) {
+	
+		this.rollno=rollno;
+		this.name=name;
+		this.age=age;
+		
+	}
+	
+}
+
+
+
